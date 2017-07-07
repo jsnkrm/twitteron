044f70e527ebe9805e34f2aad66d0354b5ed4f41
jsnkrm <m.jsnkr@gmail.com>
2017-07-08 01:08:03 +0530

    propic tweaks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var electron = require('electron')
 
 electron.app.on('ready', function(){
-    let win = new electron.BrowserWindow({width:470, height:580, frame:false, transparent:true})
+    let win = new electron.BrowserWindow({width:520, height:580, frame:false, transparent:true})
     win.loadURL('file://' + __dirname + '/index.html')
 
     win.on('resize',updateSize)
